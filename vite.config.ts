d89bf717567bf7c26dbc6b07fed62b34fe148db3
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2025-09-18 03:05:20 -0300

    feat: configure GitHub Pages deployment with workflow and base path

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      base: process.env.NODE_ENV === 'production' ? '/enem_planner/' : '/'
     };
 });
